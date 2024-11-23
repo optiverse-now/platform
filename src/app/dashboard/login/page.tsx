6a56f6288795856e2c38db8b9ec064c0939d4196
optiverse.now <optiverse.now@gmail.com>
2024-11-24 00:55:49 +0900

    名前変更

--- src/app/dashboard/login/page.tsx
+++ src/app/dashboard/login/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Page = () => {
+const Login = () => {
   return (
-    <div>Page</div>
+    <div>login</div>
   )
 }
 
-export default Page;
+export default Login;
