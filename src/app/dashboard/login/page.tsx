71f483320b337ebbd69629e325c0cbcea5b63f5f
optiverse.now <optiverse.now@gmail.com>
2024-11-24 02:19:10 +0900

    shadcnの利用とフォームのカスタマイズ

--- src/app/dashboard/login/page.tsx
+++ src/app/dashboard/login/page.tsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 
 const Login = () => {
   return (
-    <div>login</div>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="text-2xl text-center">管理者ログイン</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">メールアドレス</Label>
+                <Input id="name" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">パスワード</Label>
+                <Input id="name" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button type="submit" className="w-full">ログイン</Button>
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
 
